3eeaf222c72a7489be9376e1c933bb15675631c0
Krishna Kumar <krishna.kumar@codingworkx.com>
2024-04-15 11:23:34 +0530

    1. Added some react native text input props. 2. Added ref for text input.

--- index.tsx
+++ index.tsx
@@ -1,33 +1,48 @@
-import React, { useMemo, useState } from 'react';
-import { countryCodesData } from './countrycode';
-import ModalCloseButton from './modalCloseButton';
-import styles from './index.styles';
 import {
-    FlatList, Image, ImageStyle, Modal, Pressable, Text,
-    TextInput, TextStyle, TouchableOpacity, View, ViewStyle
-} from 'react-native';
+  FlatList,
+  Image,
+  ImageStyle,
+  Modal,
+  Pressable,
+  ReturnKeyTypeOptions,
+  Text,
+  TextInput,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+import styles from "./index.styles";
+import React, { useMemo, useState } from "react";
+import { countryCodesData } from "./countrycode";
+import ModalCloseButton from "./modalCloseButton";
 
 interface Props {
-    value: string;
-    placeHolder: string;
-    countryCode: string;
-    inputStyle?: TextStyle;
-    searchStyle?: TextStyle;
-    listItemStyle?: ViewStyle;
-    flagImgStyle?: ImageStyle;
-    containerStyle?: ViewStyle;
-    downArrowStyle?: ImageStyle;
-    listDialCodeStyle?: TextStyle;
-    listContainerStyle?: ViewStyle;
-    countryNameTextStyle?: TextStyle;
-    setSelCountry: (val: any) => void;
-    updatePhoneVal: (val: any) => void;
-    listItemSeparatorStyle?: ViewStyle;
-    modalInnerContainerStyle?: ViewStyle;
+  value: string;
+  onBlur?: () => void;
+  placeHolder: string;
+  countryCode: string;
+  inputStyle?: TextStyle;
+  searchStyle?: TextStyle;
+  listItemStyle?: ViewStyle;
+  flagImgStyle?: ImageStyle;
+  containerStyle?: ViewStyle;
+  downArrowStyle?: ImageStyle;
+  placeholderTextColor?: string;
+  listDialCodeStyle?: TextStyle;
+  listContainerStyle?: ViewStyle;
+  countryNameTextStyle?: TextStyle;
+  setSelCountry: (val: any) => void;
+  updatePhoneVal: (val: any) => void;
+  listItemSeparatorStyle?: ViewStyle;
+  modalInnerContainerStyle?: ViewStyle;
+  keyboardReturnKeyType?: ReturnKeyTypeOptions;
 }
 
-export default function PhoneNumberCountryCodeInput({
+function PhoneNumberCountryCodeInput(
+  {
     value,
+    onBlur,
     inputStyle,
     searchStyle,
     placeHolder,
@@ -40,109 +55,126 @@ export default function PhoneNumberCountryCodeInput({
     downArrowStyle,
     listDialCodeStyle,
     listContainerStyle,
+    placeholderTextColor,
     countryNameTextStyle,
+    keyboardReturnKeyType = "next",
     listItemSeparatorStyle,
-    modalInnerContainerStyle
-}: Props) {
+    modalInnerContainerStyle,
+  }: Props,
+  ref: any
+) {
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
 
-    const [open, setOpen] = useState(false);
-    const [search, setSearch] = useState("");
+  const toggleModal = () => {
+    setSearch("");
+    setOpen((val) => !val);
+  };
 
-    const toggleModal = () => {
-        setSearch("");
-        setOpen((val) => !val);
-    }
+  const getCountryCodesData = useMemo(() => {
+    return countryCodesData.filter((item) => {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [countryCodesData, search]);
 
-    const getCountryCodesData = useMemo(() => {
-        return countryCodesData.filter(item => {
-            return item.name.toLowerCase().includes(search.toLowerCase());
-        });
-    }, [countryCodesData, search]);
-
-    const renderCountryCodeModal = () => {
-        return (
-            <Modal
-                visible={open}
-                transparent={true}
-                animationType='fade'>
-                <View style={styles.modalContainer}>
-                    <View style={[styles.formContainer, modalInnerContainerStyle]}>
-                        <TextInput
-                            value={search}
-                            returnKeyType="search"
-                            placeholder="Search..."
-                            onChangeText={setSearch}
-                            style={[styles.searchStyle, searchStyle]}
-                            placeholderTextColor={'rgba(13, 1, 64, 0.60)'}
-                        />
-                        <FlatList
-                            data={getCountryCodesData}
-                            keyboardShouldPersistTaps={"handled"}
-                            renderItem={({ item }) => renderCountryCode(item)}
-                            contentContainerStyle={[styles.listContainer, listContainerStyle]}
-                        />
-                    </View>
-                    <ModalCloseButton closeModal={toggleModal} />
-                </View>
-            </Modal>
-        );
-    }
-
-    const renderCountryCode = (item: any) => {
-        return (
-            <TouchableOpacity
-                activeOpacity={0.8}
-                style={[styles.listItemCon, listItemStyle]}
-                onPress={() => {
-                    setSelCountry(item.dial_code);
-                    toggleModal();
-                }}>
-                <Image source={{ uri: item.image }} style={[styles.itemImgStyle, flagImgStyle]} />
-                <Text style={[styles.dialTextStyle, listDialCodeStyle]}>{item.dial_code}</Text>
-                <View style={[styles.seperator, listItemSeparatorStyle]} />
-                <Text numberOfLines={2} style={[styles.countryName, countryNameTextStyle]}>
-                    {item.name}
-                </Text>
-            </TouchableOpacity>
-        );
-    }
-
-    const rednerRightArrow = () => {
-        return (
-            <Image
-                resizeMode='contain'
-                resizeMethod='resize'
-                source={require("./dd_icon.png")}
-                style={[styles.ddIconStyle, downArrowStyle]}
-            />
-        );
-    }
-
+  const renderCountryCodeModal = () => {
     return (
-        <React.Fragment>
-            <View style={[styles.container, containerStyle]}>
-                <Pressable style={styles.countryCodeCon} onPress={toggleModal}>
-                    <Text style={styles.countryCodeTxt}>{countryCode}</Text>
-                    {rednerRightArrow()}
-                </Pressable>
-                <TextInput
-                    value={value}
-                    maxLength={15}
-                    returnKeyType="next"
-                    placeholder={placeHolder}
-                    keyboardType={'number-pad'}
-                    placeholderTextColor={'rgba(13, 1, 64, 0.6)'}
-                    onChangeText={(val) => {
-                        if (/^[0-9]*$/.test(val)) {
-                            updatePhoneVal(val);
-                        } else if (val.length === 0) {
-                            updatePhoneVal(val);
-                        }
-                    }}
-                    style={[styles.inputStyle, inputStyle, { fontFamily: value?.length ? '500' : '400' }]}
-                />
-            </View>
-            {renderCountryCodeModal()}
-        </React.Fragment>
+      <Modal visible={open} transparent={true} animationType="fade">
+        <View style={styles.modalContainer}>
+          <View style={[styles.formContainer, modalInnerContainerStyle]}>
+            <TextInput
+              value={search}
+              returnKeyType="search"
+              placeholder="Search..."
+              onChangeText={setSearch}
+              style={[styles.searchStyle, searchStyle]}
+              placeholderTextColor={"rgba(13, 1, 64, 0.60)"}
+            />
+            <FlatList
+              data={getCountryCodesData}
+              keyboardShouldPersistTaps={"handled"}
+              renderItem={({ item }) => renderCountryCode(item)}
+              contentContainerStyle={[styles.listContainer, listContainerStyle]}
+            />
+          </View>
+          <ModalCloseButton closeModal={toggleModal} />
+        </View>
+      </Modal>
     );
+  };
+
+  const renderCountryCode = (item: any) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={[styles.listItemCon, listItemStyle]}
+        onPress={() => {
+          setSelCountry(item.dial_code);
+          toggleModal();
+        }}
+      >
+        <Image
+          source={{ uri: item.image }}
+          style={[styles.itemImgStyle, flagImgStyle]}
+        />
+        <Text style={[styles.dialTextStyle, listDialCodeStyle]}>
+          {item.dial_code}
+        </Text>
+        <View style={[styles.seperator, listItemSeparatorStyle]} />
+        <Text
+          numberOfLines={2}
+          style={[styles.countryName, countryNameTextStyle]}
+        >
+          {item.name}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const rednerRightArrow = () => {
+    return (
+      <Image
+        resizeMode="contain"
+        resizeMethod="resize"
+        source={require("./dd_icon.png")}
+        style={[styles.ddIconStyle, downArrowStyle]}
+      />
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <View style={[styles.container, containerStyle]}>
+        <Pressable style={styles.countryCodeCon} onPress={toggleModal}>
+          <Text style={styles.countryCodeTxt}>{countryCode}</Text>
+          {rednerRightArrow()}
+        </Pressable>
+        <TextInput
+          value={value}
+          ref={ref}
+          maxLength={15}
+          onBlur={onBlur}
+          returnKeyType={keyboardReturnKeyType}
+          placeholder={placeHolder}
+          keyboardType={"number-pad"}
+          placeholderTextColor={placeholderTextColor}
+          onChangeText={(val) => {
+            if (/^[0-9]*$/.test(val)) {
+              updatePhoneVal(val);
+            } else if (val.length === 0) {
+              updatePhoneVal(val);
+            }
+          }}
+          style={[
+            styles.inputStyle,
+            { fontFamily: value?.length ? "500" : "400" },
+            inputStyle,
+          ]}
+        />
+      </View>
+      {renderCountryCodeModal()}
+    </React.Fragment>
+  );
 }
+
+export default React.forwardRef(PhoneNumberCountryCodeInput);
